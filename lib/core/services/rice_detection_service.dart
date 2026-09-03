2f440640f7ca969782bbb72cc38f40302e1e9dc2
sheezaShahzad <sheezasheeza938@gmail.com>
2026-09-03 18:06:57 +0500

    testing

--- lib/core/services/rice_detection_service.dart
+++ lib/core/services/rice_detection_service.dart
@@ -1,7 +1,7 @@
-
 import 'dart:convert';
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 import '../../models/rice_detection_result.dart';
 
 class RiceDetectionService {
@@ -13,8 +13,17 @@ class RiceDetectionService {
     final request = http.MultipartRequest("POST", uri);
     request.headers['Accept'] = 'application/json';
 
+    // Determine mime type from file extension (web bytes often lose this info)
+    final ext = fileName.split('.').last.toLowerCase();
+    final mimeSubtype = (ext == 'jpg' || ext == 'jpeg') ? 'jpeg' : 'png';
+
     request.files.add(
-      http.MultipartFile.fromBytes("image", imageBytes, filename: fileName),
+      http.MultipartFile.fromBytes(
+        "image",
+        imageBytes,
+        filename: fileName.contains('.') ? fileName : '$fileName.jpg',
+        contentType: MediaType('image', mimeSubtype),
+      ),
     );
 
     final streamedResponse = await request.send();
